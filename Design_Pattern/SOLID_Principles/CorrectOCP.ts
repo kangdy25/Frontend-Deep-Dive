3d7fedd0b09c7e7f0cc7fe6cd0ba0ef6507c80af
Kangdy <canfrog@naver.com>
2026-03-07 11:29:05 +0900

    [Design Pattern] Learn OCP(개방-폐쇄 원칙)

---
+++ Design_Pattern/SOLID_Principles/CorrectOCP.ts
@@ -0,0 +1,30 @@
+// ✅ OCP를 적용한 코드
+interface Report {
+  generate(): void;
+}
+
+class PDFReport implements Report {
+  generate(): void {
+    console.log("Generating PDF report...");
+  }
+}
+
+class HTMLReport implements Report {
+  generate(): void {
+    console.log("Generating HTML report...");
+  }
+}
+
+class XMLReport implements Report {
+  generate(): void {
+    console.log("Generating XML report...");
+  }
+}
+
+const pdfReport: Report = new PDFReport();
+const htmlReport: Report = new HTMLReport();
+const xmlReport: Report = new XMLReport();
+
+pdfReport.generate();
+htmlReport.generate();
+xmlReport.generate();
---
+++ Design_Pattern/SOLID_Principles/WrongOCP.ts
@@ -0,0 +1,17 @@
+// ⛔️ OCP를 위반한 코드
+
+class ReportGenerator {
+  public generateReport(type: string): void {
+    if (type === "PDF") {
+      console.log("Generating PDF report...");
+    } else if (type === "HTML") {
+      console.log("Generating HTML report...");
+    }
+    // 새로운 형식을 추가하려면 이 메서드 내부를 수정해야 합니다. (OCP 위반 상태 유지)
+  }
+}
+
+// 사용 예시
+const generator = new ReportGenerator();
+generator.generateReport("PDF");
+generator.generateReport("HTML");
