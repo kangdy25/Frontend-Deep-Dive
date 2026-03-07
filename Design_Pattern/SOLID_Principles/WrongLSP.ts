f1cd7b1356d8f43a927ed356c7cdedaa458606a8
Kangdy <canfrog@naver.com>
2026-03-07 12:45:12 +0900

    [Design Pattern] Learn LSP(리스코프 치환 원칙)

---
+++ Design_Pattern/SOLID_Principles/WrongLSP.ts
@@ -0,0 +1,17 @@
+class Bird {
+  fly(): void {
+    console.log("Bird is flying");
+  }
+}
+
+class Penguin extends Bird {
+  fly(): void {
+    throw new Error("Penguins cannot fly");
+  }
+}
+
+const bird: Bird = new Bird();
+bird.fly();
+
+const penguin: Bird = new Penguin();
+penguin.fly();
